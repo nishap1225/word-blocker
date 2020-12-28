2edf4e4c4ea46953345aba81354b330a7a252e8f
nishap1225 <nishap1225@gmail.com>
2020-12-28 13:31:02 -0800

    popup accepting input

---
+++ background.js
@@ -0,0 +1,19 @@
+console.log("from background")
+let active_tab_id = 0;
+chrome.tabs.onActivated.addListener(tab => {
+    chrome.tabs.get(tab.tabId, current_tab_info => {
+          active_tab_id = tab.tabId;
+          chrome.tabs.insertCSS(null, {file: "./styles.css"});
+          chrome.tabs.executeScript(null, {file: "./foreground.js"}, () => console.log("i injected")) 
+    });
+}); //need to look for all the cases later, i.e. refresh the tab
+
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message == 'yo check the storage') {
+    chrome.tabs.sendMessage(active_tab_id, {message: 'yo i got your message'});
+    chrome.storage.local.get("password", value=> {
+      console.log(value);
+    })
+  }
+})
