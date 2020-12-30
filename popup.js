37ab68b53bc8125e62f150dff7ddc44118d13382
nishap1225 <nishap1225@gmail.com>
2020-12-30 15:31:48 -0800

    started creating dynamic list

--- popup.js
+++ popup.js
@@ -1,7 +1,18 @@
 function newWord() {
   var word = document.getElementById("new_word").value; 
   //document.getElementById("demo").innerHTML = word; 
-  
+
+}
+
+function populateList() {
+  var ul = document.getElementById("list");
+  for (var i = 0; i < toppings.length; i++) { //fix 
+    var word = toppings[i]; //fix 
+
+    var listItem = document.createElement("li");
+    listItem.textContent = word;
+    ul.appendChild(listItem);
+  }
 }
 
 window.onload = function() {
