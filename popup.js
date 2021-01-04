0a5f97e2326063e75004b2e7d9b90e2b974e7ae3
Reena Yuan <reenayuan@berkeley.edu>
2021-01-04 17:21:55 -0600

    figured out storage

--- popup.js
+++ popup.js
@@ -1,6 +1,31 @@
 function newWord() {
   var word = document.getElementById("new_word").value; 
-  //document.getElementById("demo").innerHTML = word; 
+  //document.getElementById("demo").innerHTML = word;
+  //add word to storage
+  chrome.storage.sync.set({word: word}, function() {
+    console.log('Value is set to ' + word);
+  });
+  chrome.storage.sync.set({'two': 'test2'}, function() {
+    console.log('Value is set to ' + 'test2');
+  });
+
+  // chrome.storage.local.get([word], function(result) {
+  //   console.log('Value currently is ' + result.word);
+  // });
+
+  // chrome.storage.sync.set({key: value}, function() {
+  //   console.log('Value is set to ' + value);
+  // });
+  
+  chrome.storage.sync.get(null, function(result) {
+    console.log('Value currently is ' + result);
+    for (var key in result) {
+      var storageItem = result[key];
+      console.log('item' + storageItem);
+    }
+
+  });
+
 
 }
 
@@ -24,4 +49,19 @@ window.onload = function() {
 }
 
   //add event listener in popup.js for a button 
+
+  chrome.storage.onChanged.addListener(function(changes, namespace) {
+    console.log('hello');
+    for (var key in changes) {
+      var storageChange = changes[key];
+      console.log('Storage key "%s" in namespace "%s" changed. ' +
+                  'Old value was "%s", new value is "%s".',
+                  key,
+                  namespace,
+                  storageChange.oldValue,
+                  storageChange.newValue);
+    }
+    //populateList();
+  });
+
   //send the data to storage 
