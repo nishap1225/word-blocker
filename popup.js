07a89b973039ac9f537241ee395e122c6326199c
nishap1225 <nishap1225@gmail.com>
2021-01-05 13:10:28 -0800

    finished popup:)

--- popup.js
+++ popup.js
@@ -1,67 +1,91 @@
 function newWord() {
   var word = document.getElementById("new_word").value; 
-  //document.getElementById("demo").innerHTML = word;
-  //add word to storage
-  chrome.storage.sync.set({word: word}, function() {
-    console.log('Value is set to ' + word);
-  });
-  chrome.storage.sync.set({'two': 'test2'}, function() {
-    console.log('Value is set to ' + 'test2');
-  });
-
-  // chrome.storage.local.get([word], function(result) {
-  //   console.log('Value currently is ' + result.word);
-  // });
-
-  // chrome.storage.sync.set({key: value}, function() {
-  //   console.log('Value is set to ' + value);
-  // });
-  
-  chrome.storage.sync.get(null, function(result) {
-    console.log('Value currently is ' + result);
-    for (var key in result) {
-      var storageItem = result[key];
-      console.log('item' + storageItem);
+  chrome.storage.sync.get(null, function(result) { //key is "words" for the array of words 
+    var storageArray = result['words']; 
+    if (storageArray == null) {
+      storageArray = [word]; 
+    } else {
+      if (!storageArray.includes(word)) {
+        //append to the popup
+        var ul = document.getElementById("list");
+        var listItem = document.createElement("li");
+        listItem.textContent = word;
+        listItem.id = word; 
+        ul.appendChild(listItem); 
+        storageArray.push(word); 
+      }
     }
-
+    chrome.storage.sync.set({'words': storageArray}, function() {
+      console.log('Array is set to ' + storageArray);
+    }); //later, make it so that there is no callback function 
   });
-
-
 }
 
-function populateList() {
-  var ul = document.getElementById("list");
-  for (var i = 0; i < toppings.length; i++) { //fix 
-    var word = toppings[i]; //fix 
-
-    var listItem = document.createElement("li");
-    listItem.textContent = word;
-    ul.appendChild(listItem);
+function deleteWord() {
+  var word = document.getElementById("new_word").value; 
+  chrome.storage.sync.get(null, function(result) { //key is "words" for the array of words 
+  var storageArray = result['words']; 
+  if (storageArray != null && storageArray.includes(word)) {
+    var ul = document.getElementById("list");
+    var li = document.getElementById(word);
+    console.log(ul); 
+    console.log(li); 
+    ul.removeChild(li); 
+    var index = storageArray.indexOf(word); 
+    storageArray.splice(index, 1); 
+    chrome.storage.sync.set({'words': storageArray}, function() {
+      console.log('Array is set to ' + storageArray);
+    });
   }
+});
 }
 
 window.onload = function() {
+  populateList(); //i think this should be called instead of new word 
   var button = document.getElementById("submit");
   if (button.addEventListener)
     button.addEventListener("click", newWord, false);
   else if (button.attachEvent)
     button.attachEvent('onclick', newWord);
+  
+  var delete_button = document.getElementById("delete");
+  if (delete_button.addEventListener)
+    delete_button.addEventListener("click", deleteWord, false);
+  else if (delete_button.attachEvent)
+    delete_button.attachEvent('onclick', deleteWord);
+  
 }
 
-  //add event listener in popup.js for a button 
-
-  chrome.storage.onChanged.addListener(function(changes, namespace) {
-    console.log('hello');
-    for (var key in changes) {
-      var storageChange = changes[key];
-      console.log('Storage key "%s" in namespace "%s" changed. ' +
-                  'Old value was "%s", new value is "%s".',
-                  key,
-                  namespace,
-                  storageChange.oldValue,
-                  storageChange.newValue);
+function populateList() { //called at the beginning 
+  var ul = document.getElementById("list");
+  chrome.storage.sync.get(null, function(result) { //inputting null gets everything in the storage
+    var storageArray = result['words']; 
+    for (i = 0; i < storageArray.length; i++) {
+      var word = storageArray[i]; 
+      var listItem = document.createElement("li");
+      listItem.textContent = word;
+      listItem.id = word; 
+      ul.appendChild(listItem);
     }
-    //populateList();
   });
+}
+
+
+
+//   //add event listener in popup.js for a button 
+
+//   chrome.storage.onChanged.addListener(function(changes, namespace) {
+//     console.log('hello');
+//     for (var key in changes) {
+//       var storageChange = changes[key];
+//       console.log('Storage key "%s" in namespace "%s" changed. ' +
+//                   'Old value was "%s", new value is "%s".',
+//                   key,
+//                   namespace,
+//                   storageChange.oldValue,
+//                   storageChange.newValue);
+//     }
+//     //populateList();
+//   });
 
   //send the data to storage 
