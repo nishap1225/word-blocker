7bb3cbe1b5541e83b78da98279f8e34d77b80ddf
Reena Yuan <reenayuan@berkeley.edu>
2021-01-07 15:24:36 -0600

    no work

--- contentScript.js
+++ contentScript.js
@@ -8,21 +8,42 @@
 //   });
 
 document.body.querySelectorAll('*').forEach(function(node) {
+    console.log('NEW NODE');
     chrome.storage.sync.get(null, function(result) {  
     var storageArray = result['words']; 
+    var nodeLength = node.innerText.length;
     if (storageArray != null) {
         for (var i = 0; i < storageArray.length; i++) {
-            var index = node.innerText.indexOf(storageArray[i]);  //innerHTML
-            if(index !== -1) {
+            var length = storageArray[i].length; 
+            var censor = new Array(length+1).join('*');
+            var textSearch = node.innerText;
+            var searchLength = nodeLength;
+            //var wholeIndex = 0;
+            var index = textSearch.indexOf(storageArray[i]);
+            var check = index;
+            //while (index !== -1) {
+            while (check !== -1) {
+                  //innerHTML
+                //wholeIndex += index;
+  
                 console.log("Word reached!");
                 // ***** 
                 // var length = node.innerText.length; 
                 // var censor = new Array(length + 1).join('*');
                 // node.innerText = censor; 
-                var length = storageArray[i].length; 
-                var censor = new Array(length+1).join('*');
+                //index += nodeLength - searchLength;
+                
+                //node.innerText = node.innerText.slice(0, wholeIndex) + censor + node.innerText.slice(wholeIndex + length);
+                console.log('node', node.innerText.slice(index, index + length));
                 node.innerText = node.innerText.slice(0, index) + censor + node.innerText.slice(index + length);
-            }
+                textSearch = node.innerText.slice(index + length);
+                searchLength = textSearch.length;
+                //index = textSearch.indexOf(storageArray[i]);
+                check = textSearch.indexOf(storageArray[i]);
+                index += check;
+                //wholeIndex += index + length;
+                
+            }  
         }
     }
     });
