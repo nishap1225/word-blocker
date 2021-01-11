f8a5470269b7b98a8ab7a6fd89f4fb1c38ac79b8
nishap1225 <nishap1225@gmail.com>
2021-01-11 14:04:28 -0800

    trying to preserve style

--- background.js
+++ background.js
@@ -3,18 +3,18 @@ let active_tab_id = 0;
 chrome.tabs.onActivated.addListener(tab => {
     chrome.tabs.get(tab.tabId, current_tab_info => {
           active_tab_id = tab.tabId;
-          chrome.tabs.insertCSS(null, {file: "./styles.css"});
+          //chrome.tabs.insertCSS(null, {file: "./styles.css"});
           //chrome.tabs.executeScript(null, {file: "./foreground.js"}, () => console.log("i injected"))
           chrome.tabs.executeScript(null, {file: "./contentScript.js"}, () => console.log("i injected content"))
     });
 }); //need to look for all the cases later, i.e. refresh the tab
 
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.message == 'yo check the storage') {
-    chrome.tabs.sendMessage(active_tab_id, {message: 'yo i got your message'});
-    chrome.storage.local.get("password", value=> {
-      console.log(value);
-    })
-  }
-})
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   if (request.message == 'yo check the storage') {
+//     chrome.tabs.sendMessage(active_tab_id, {message: 'yo i got your message'});
+//     chrome.storage.local.get("password", value=> {
+//       console.log(value);
+//     })
+//   }
+// })
--- contentScript.js
+++ contentScript.js
@@ -1,52 +1,89 @@
-// var script = document.createElement('script');
-// script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
-// script.type = 'text/javascript';
-// document.getElementsByTagName('head')[0].appendChild(script);
-
-// $(document).ready(function () {
-//     $( "div:contains('John')" ).css( "text-decoration", "underline" );
-//   });
-
-document.body.querySelectorAll('*').forEach(function(node) {
-    console.log('NEW NODE');
-    chrome.storage.sync.get(null, function(result) {  
-    var storageArray = result['words']; 
-    var nodeLength = node.innerText.length;
-    if (storageArray != null) {
-        for (var i = 0; i < storageArray.length; i++) {
-            var length = storageArray[i].length; 
-            var censor = new Array(length+1).join('*');
-            var textSearch = node.innerText;
-            var searchLength = nodeLength;
-            //var wholeIndex = 0;
-            var index = textSearch.indexOf(storageArray[i]);
-            var check = index;
-            //while (index !== -1) {
-            while (check !== -1) {
-                  //innerHTML
-                //wholeIndex += index;
+// document.body.querySelectorAll('*').forEach(function(node) {
+//     chrome.storage.sync.get(null, function(result) {  
+//     var storageArray = result['words']; 
+//     var nodeLength = node.innerText.length;
+//     if (storageArray != null) {
+//         for (var i = 0; i < storageArray.length; i++) {
+//             var length = storageArray[i].length; 
+//             var censor = new Array(length+1).join('*');
+//             var textSearch = node.innerText;
+//             var searchLength = nodeLength;
+//             //var wholeIndex = 0;
+//             var index = textSearch.indexOf(storageArray[i]);
+//             var check = index;
+//             //while (index !== -1) {
+//             while (check !== -1) {
+//                   //innerHTML
+//                 //wholeIndex += index;
   
-                console.log("Word reached!");
-                // ***** 
-                // var length = node.innerText.length; 
-                // var censor = new Array(length + 1).join('*');
-                // node.innerText = censor; 
-                //index += nodeLength - searchLength;
+//                 console.log("Word reached!");
+//                 // ***** 
+//                 // var length = node.innerText.length; 
+//                 // var censor = new Array(length + 1).join('*');
+//                 // node.innerText = censor; 
+//                 //index += nodeLength - searchLength;
                 
-                //node.innerText = node.innerText.slice(0, wholeIndex) + censor + node.innerText.slice(wholeIndex + length);
-                console.log('node', node.innerText.slice(index, index + length));
-                node.innerText = node.innerText.slice(0, index) + censor + node.innerText.slice(index + length);
-                textSearch = node.innerText.slice(index + length);
-                searchLength = textSearch.length;
-                //index = textSearch.indexOf(storageArray[i]);
-                check = textSearch.indexOf(storageArray[i]);
-                index += check;
-                //wholeIndex += index + length;
+//                 //node.innerText = node.innerText.slice(0, wholeIndex) + censor + node.innerText.slice(wholeIndex + length);
+//                 console.log('node', node.innerText.slice(index, index + length));
+//                 node.innerText = node.innerText.slice(0, index) + censor + node.innerText.slice(index + length);
+//                 textSearch = node.innerText.slice(index + length);
+//                 searchLength = textSearch.length;
+//                 //index = textSearch.indexOf(storageArray[i]);
+//                 check = textSearch.indexOf(storageArray[i]);
+//                 index += check;
+//                 //wholeIndex += index + length;
                 
-            }  
+//             }  
+//         }
+//     }
+//     });
+// });
+
+var elements = document.getElementsByTagName('*');
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i]; 
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j]; 
+        if (node.nodeType === 3) {
+            var storageArray; 
+            chrome.storage.sync.get(null, function(result) {  
+                storageArray = result['words']; 
+            });
+            var text = node.nodeValue; 
+            for (var i = 0; i < storageArray.length; i++) {
+                var word = storageArray[i]; 
+                var length = storageArray[i].length; 
+                var censor = new Array(length+1).join('*');
+                var replacedText = text.replace(word,censor);
+
+                if (replaced != text) {
+                    element.replaceChild()
+                }
+
+            }
         }
     }
-    });
-});
-
+}
 //things to worry about: case sensitivity; doesn't work websites; kind of unreliable :) 
+
+//Add a new function to the HTMLElement object so it cna be used on any HTMLElement
+// HTMLElement.prototype.textNodes = function() {
+//     return [...this.childNodes].filter((node) => {
+//       return (node.nodeType === Node.TEXT_NODE && node.nodeValue.trim() !== "");
+//     });
+//   }
+  
+//   //Use the new HTMLElement function
+//   document.addEventListener('DOMContentLoaded', () => {
+//     document.querySelector('#replaceAll').addEventListener('click', () => {
+//       document.querySelector('#testSubject').textNodes().forEach((node) => {
+//         console.log(node.textContent);
+//         node.textContent = 'Replaced';
+//       });
+//     });
+  
+//     document.querySelector('#replaceFirst').addEventListener('click', function() {
+//       document.querySelector('#testSubject').textNodes()[0].textContent = 'Replaced First';
+//     });
+//   });
+  
