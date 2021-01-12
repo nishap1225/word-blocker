91f35ab075af6d506e3c0c684324c2573784ac11
nishap1225 <nishap1225@gmail.com>
2021-01-12 12:09:37 -0800

    basic working

--- contentScript.js
+++ contentScript.js
@@ -39,34 +39,35 @@
 //     });
 // });
 
-var elements = document.getElementsByTagName('*');
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i]; 
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j]; 
-        if (node.nodeType === 3) {
-            var storageArray; 
-            chrome.storage.sync.get(null, function(result) {  
-                storageArray = result['words']; 
-            });
-            console.log('got storage array');
-            if (storageArray != null) {
-                //console.log('array is not null');
-                var text = node.nodeValue; 
-                for (var i = 0; i < storageArray.length; i++) {
-                    var word = storageArray[i]; 
-                    var length = storageArray[i].length; 
-                    var censor = new Array(length+1).join('*');
-                    var replacedText = text.replace(word,censor);
-                    if (replacedText !== text) { //replacedText.localeCompare(text) != 0
-                        console.log('replacing text');
-                        element.replaceChild(document.createTextNode(replacedText), node);
-                    }
-                } 
+chrome.storage.sync.get(null, function(result) {  
+    var storageArray = result['words']; 
+    console.log(storageArray); 
+    if (storageArray != null) {
+        var elements = document.getElementsByTagName('*');
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i]; 
+            for (var j = 0; j < element.childNodes.length; j++) {
+                var node = element.childNodes[j]; 
+                if (node.nodeType === 3) {
+                    var text = node.nodeValue; 
+                    for (var k = 0; k < storageArray.length; k++) {
+                        var word = storageArray[k]; 
+                        var length = word.length; 
+                        var censor = new Array(length+1).join('*');
+                        var regEx = new RegExp(word, "ig"); 
+                        var replacedText = text.replaceAll(regEx,censor);
+                        if (replacedText !== text) { //replacedText.localeCompare(text) != 0
+                            console.log('replacing text');
+                            element.replaceChild(document.createTextNode(replacedText), node);
+                        }
+                    } 
+                }
             }
         }
     }
-}
+});
+
+
 //things to worry about: case sensitivity; doesn't work websites; kind of unreliable :) 
 
 //Add a new function to the HTMLElement object so it cna be used on any HTMLElement
